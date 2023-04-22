1a77880fc3b87c4c62c85d319bf773f21e2707b6
Leo450 <leo450450@gmail.com>
2023-04-22 17:06:10 +0200

    qzd

--- Camera.gd
+++ Camera.gd
@@ -2,20 +2,14 @@ extends Camera2D
 
 var isMoving = false
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	is_current()
-	Events.connect("translate_camera", translateCamera)
+	Events.connect("translate_camera", translate_camera)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-func translateCamera(offset):
+func translate_camera(direction):
 	if not isMoving:
 		isMoving = true
-		await create_tween().tween_property(self, "global_position", global_position + offset, 0.5).finished
+		await create_tween().tween_property(self, "global_position", global_position + direction * Vector2(320, 180), 0.5).finished
 		isMoving = false
-#	else:
-#		await get_tree().idl
+		
 	Events.emit_signal("finished_translation")
--- TriggerCamera.gd
+++ TriggerCamera.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
-@export var offset = Vector2.ZERO
+@export var is_horizontal = true
+
 var can_trigger = true
 
 # Called when the node enters the scene tree for the first time.
@@ -12,17 +13,18 @@ func _process(delta):
 	pass
 
 func _on_body_entered(body):
-	if can_trigger:
-		can_trigger = false
+	if !can_trigger:
+		return
 		
-		Events.emit_signal("translate_camera", offset)
-		
-		if offset.x != 0:
-			body.global_position.x = global_position.x + sign(offset.x) * 8
-		if offset.y != 0:
-			body.global_position.y = global_position.y + sign(offset.y) * 8
-			
-		offset *= -1
-		
-		await Events.finished_translation
-		can_trigger = true
+	can_trigger = false
+	
+	var direction = body.last_input_vector
+	if (is_horizontal):
+		direction.x = 0
+	else:
+		direction.y = 0
+	
+	Events.emit_signal("translate_camera", direction)
+	
+	await Events.finished_translation
+	can_trigger = true
